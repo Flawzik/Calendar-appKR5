b79748561d9d9d63fc1a97a4967273aeb3c59ee2
Flawzik <79632599+Flawzik@users.noreply.github.com>
2025-12-10 00:59:21 +0300

    Delete calendar-app directory

--- calendar-app/src/middleware/loggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-const loggerMiddleware = (req, res, next) => {
-  const timestamp = new Date().toLocaleString();
-  console.log(`[${timestamp}] ${req.method} ${req.url} - IP: ${req.ip}`);
-  next(); // Передаем управление следующему middleware или route
-};
-
-module.exports = loggerMiddleware;
